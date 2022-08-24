e9d816429c5da425336db9981bc51de7f0045b9b
WEGASS <rden2000@gmail.com>
2022-08-24 18:12:32 +0300

    Deploy to Heroku

--- btctest/settings.py
+++ btctest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['https://faucet-btc-testnet.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['faucet-btc-testnet.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
